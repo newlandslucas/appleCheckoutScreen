1e429b5301d151973493b8b84ccb5b6652a569dd
C1326386 Lucas Silveira Newlands Machado <lucas.newlands@ibm.com>
2023-02-20 16:48:48 -0300

    feature: Bottom Retangle component

--- src/Screen/Home/index.tsx
+++ src/Screen/Home/index.tsx
@@ -1,13 +1,24 @@
-import { Text } from 'react-native';
-import { BottomRetangle, Container, ProductTitle, ProductSubTitle, ProductDescription} from './styles';
+import { BottomRetangle, Container, ProductTitle, ProductDescription, ProductText, Footer, PriceTag, ButtonTitle, PurchaseButton} from './styles';
+import { Entypo } from '@expo/vector-icons'; 
+
 
 export function Home() {
     return(
         <Container>
             <BottomRetangle>
                 <ProductTitle>HOMEPOD</ProductTitle>
-                <ProductSubTitle>2nd generation</ProductSubTitle>
                 <ProductDescription>Immersive, hight-fidelity audio.</ProductDescription>
+                <ProductText>HomePod is a powerhouse of a speaker. Apple‑engineered audio technology and advanced software deliver high‑fidelity sound throughout the room. It intelligently adapts to whatever it’s playing — or wherever it’s playing — and surrounds you in immersive audio that makes everything you listen to sound incredible.</ProductText>
+
+                <Footer>
+                    <PriceTag>$ 299.00</PriceTag>
+                    <PurchaseButton>
+                        <ButtonTitle>Add to cart</ButtonTitle>
+                        <Entypo name="chevron-small-right" size={22} color="white" />
+                    </PurchaseButton>
+                </Footer>
+
+
             </BottomRetangle>
         </Container>
     )
--- src/Screen/Home/styles.ts
+++ src/Screen/Home/styles.ts
@@ -11,8 +11,55 @@ export const BottomRetangle = styled.View`
     height: 375px;
     border-radius: 40px;
     background-color: #2E2F34;
+    padding: 24px;
 `;
 
-export const ProductTitle = styled.Text``;
-export const ProductSubTitle = styled.Text``;
-export const ProductDescription = styled.Text``;
+export const ProductTitle = styled.Text`
+    font-size: 30px;
+    font-family:  Montserrat_700Bold;
+    color: #fff;
+`;
+
+export const ProductDescription = styled.Text`
+    font-family: Montserrat_700Bold;
+    font-size: 13px;
+    color: #fff;
+`;
+
+export const ProductText = styled.Text`
+    font-family: Montserrat_400Regular;
+    font-size: 14px;
+    color: #fff;
+    margin-top: 20px;
+`;
+
+export const Footer = styled.View`
+    width: 100%;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    margin-top: auto;
+    margin-bottom: 15px;
+`;
+
+export const PriceTag = styled.Text`
+    font-family:  Montserrat_700Bold;
+    font-size: 24px;
+    color: #FFF;
+`;
+
+export const PurchaseButton = styled.TouchableOpacity`
+    width: 150px;
+    height: 50px;
+    background-color: #007aff;
+    border-radius: 30px;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const ButtonTitle = styled.Text`
+    font-family: Montserrat_700Bold;
+    font-size: 14px;
+    color: #fff;
+`;
